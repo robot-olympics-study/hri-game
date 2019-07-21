a4b6b532a466f34c1868ed7545ccbdcef2896a43
matthew29tang <matthew29tang@gmail.com>
2019-07-21 16:41:28 -0700

    Updates

---
+++ precache-manifest.5b384aa7e023767b04d87cc24a7874fb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4304b3219db55e360de398f033128b2",
+    "url": "/hri-game/index.html"
+  },
+  {
+    "revision": "3ded54a8ea0df9d266c1",
+    "url": "/hri-game/static/css/main.82d4b501.chunk.css"
+  },
+  {
+    "revision": "891967b5d98b6bcceea5",
+    "url": "/hri-game/static/js/2.1e3d3338.chunk.js"
+  },
+  {
+    "revision": "3ded54a8ea0df9d266c1",
+    "url": "/hri-game/static/js/main.7c194c70.chunk.js"
+  },
+  {
+    "revision": "5dc05d134a730881d857",
+    "url": "/hri-game/static/js/runtime~main.058b036c.js"
+  },
+  {
+    "revision": "a57c110016b284595489b31eb3b08feb",
+    "url": "/hri-game/static/media/Room0.a57c1100.PNG"
+  },
+  {
+    "revision": "397b61bce1cff7d0eda587adb123cf71",
+    "url": "/hri-game/static/media/Room1.397b61bc.PNG"
+  },
+  {
+    "revision": "750261addd6acae579a8fca12ec5b021",
+    "url": "/hri-game/static/media/Room2.750261ad.PNG"
+  },
+  {
+    "revision": "6dcde804fe7d584d78f139b9456c0d80",
+    "url": "/hri-game/static/media/Room3.6dcde804.PNG"
+  },
+  {
+    "revision": "c64aeb0861b5cca5121538a23e64dd70",
+    "url": "/hri-game/static/media/Room4.c64aeb08.PNG"
+  },
+  {
+    "revision": "34f00fd2b474a427ae4f1b4774394cec",
+    "url": "/hri-game/static/media/Room5.34f00fd2.PNG"
+  },
+  {
+    "revision": "5307f2fd16d104c7daae45191baade6f",
+    "url": "/hri-game/static/media/Room6.5307f2fd.PNG"
+  },
+  {
+    "revision": "f6f054c2cd7befeabfa9b3893968bcb4",
+    "url": "/hri-game/static/media/Room7.f6f054c2.PNG"
+  }
+]);
