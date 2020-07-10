760f794ecb000790cac78967095ef573281e9b45
saurabhk7 <saurabhkshirsagar35@gmail.com>
2020-07-09 23:08:14 -0700

    Added new gameboards for Greedy Only

--- src/pages/config.js
+++ src/pages/config.js
@@ -4,7 +4,7 @@ import { robot_actionsB } from './robot_actionsB.js';
 import { robot_actionsBA } from './robot_actionsBA.js';
 
 const DEBUG_MODE = false;
-const SITE_VERSION = 2.14;
+const SITE_VERSION = 2.15;
 
 const rewards1 = [1.0, 3.0, 2.5, 2.0]
 const rewards2 = [8.0, 10.0, 7.5, 6.0]
@@ -37,29 +37,50 @@ const rooms = {
   }
 
 //STRATEGY FOR THE GAME
-var STRATEGY = "MCTS"
-var strategy_probability = Math.random()
-if (strategy_probability > 0.5) {
-  STRATEGY = 'MCTS'
-}
-else{
-  STRATEGY = 'GREEDY'
-}
+var STRATEGY = "GREEDY"
+// var strategy_probability = Math.random()
+// if (strategy_probability > 0.5) {
+//   STRATEGY = 'MCTS'
+// }
+// else{
+//   STRATEGY = 'GREEDY'
+// }
 
 const introVideoUrl = "https://www.youtube.com/watch?v=mkJ5-4U1VyU"
 
+//OLD CODE
+// // BEGIN SET TREATMENT
+// var TREATMENT = 'BA'
+// // Example of random treatment. Not compatible with restarting from cookies.
+// var treatment_probability = Math.random()
+// if (treatment_probability > 0.75 && treatment_probability < 1) {
+//   TREATMENT = 'AB'
+// } else if (treatment_probability > 0.5 && treatment_probability < 0.75) {
+//   TREATMENT = 'A'
+// } else if (treatment_probability > 0.25 && treatment_probability < 0.5) {
+//   TREATMENT = 'B'
+// } else if (treatment_probability > 0 && treatment_probability < 0.25) {
+//   TREATMENT = 'BA'
+// }
+// // END SET TREATMENT
+
+//NEW CODE
 // BEGIN SET TREATMENT
-var TREATMENT = 'BA'
+var TREATMENT = 'AB1'
 // Example of random treatment. Not compatible with restarting from cookies.
 var treatment_probability = Math.random()
-if (treatment_probability > 0.75 && treatment_probability < 1) {
-  TREATMENT = 'AB'
-} else if (treatment_probability > 0.5 && treatment_probability < 0.75) {
-  TREATMENT = 'A'
-} else if (treatment_probability > 0.25 && treatment_probability < 0.5) {
-  TREATMENT = 'B'
-} else if (treatment_probability > 0 && treatment_probability < 0.25) {
-  TREATMENT = 'BA'
+if (treatment_probability >= 0.84 && treatment_probability < 1) {
+  TREATMENT = 'AB1'
+} else if (treatment_probability >= 0.67 && treatment_probability < 0.84) {
+  TREATMENT = 'AB2'
+} else if (treatment_probability >= 0.5 && treatment_probability < 0.67) {
+  TREATMENT = 'AB4'
+} else if (treatment_probability >= 0.34 && treatment_probability < 0.5) {
+  TREATMENT = 'BA1'
+} else if (treatment_probability >= 0.167 && treatment_probability < 0.34) {
+  TREATMENT = 'BA3'
+} else if (treatment_probability > 0 && treatment_probability < 0.167) {
+  TREATMENT = 'BA4'
 }
 // END SET TREATMENT
 
@@ -68,23 +89,50 @@ var robot_strategy = null;
 var roomList = [[0,1,2,3], [4,5,6,7], [8,9,10,11], [12,13,14,15], [16,17,18,19]];
 var roomOrder = null;
 var MAX_ROOMS = null;
+//OLD CODE
+// if (TREATMENT === 'AB') {
+//   robot_strategy = robot_actionsAB;
+//   roomOrder = [0, 1, 2, 3, 4];
+//   MAX_ROOMS = 5;
+// } else if (TREATMENT === 'A') {
+//   robot_strategy = robot_actionsA;
+//   roomOrder = [0, 1, 2];
+//   MAX_ROOMS = 3;
+// } else if (TREATMENT === 'B') {
+//   robot_strategy = robot_actionsB;
+//   roomOrder = [3, 4];
+//   MAX_ROOMS = 2;
+// } else if (TREATMENT === 'BA') {
+//   robot_strategy = robot_actionsBA;
+//   roomOrder = [3, 4, 0, 1, 2];
+//   MAX_ROOMS = 5;
+// }
 
-if (TREATMENT === 'AB') {
+//NEW CODE
+if (TREATMENT === 'AB1') {
   robot_strategy = robot_actionsAB;
-  roomOrder = [0, 1, 2, 3, 4];
-  MAX_ROOMS = 5;
-} else if (TREATMENT === 'A') {
+  roomOrder = [0];
+  MAX_ROOMS = 1;
+} else if (TREATMENT === 'AB2') {
   robot_strategy = robot_actionsA;
-  roomOrder = [0, 1, 2];
-  MAX_ROOMS = 3;
-} else if (TREATMENT === 'B') {
-  robot_strategy = robot_actionsB;
-  roomOrder = [3, 4];
+  roomOrder = [0, 1];
   MAX_ROOMS = 2;
-} else if (TREATMENT === 'BA') {
+} else if (TREATMENT === 'AB4') {
+  robot_strategy = robot_actionsB;
+  roomOrder = [0, 1, 2, 3];
+  MAX_ROOMS = 4;
+} else if (TREATMENT === 'BA1') {
   robot_strategy = robot_actionsBA;
-  roomOrder = [3, 4, 0, 1, 2];
-  MAX_ROOMS = 5;
+  roomOrder = [3];
+  MAX_ROOMS = 1;
+} else if (TREATMENT === 'BA3') {
+  robot_strategy = robot_actionsB;
+  roomOrder = [3, 4, 0];
+  MAX_ROOMS = 3;
+} else if (TREATMENT === 'BA4') {
+  robot_strategy = robot_actionsB;
+  roomOrder = [3, 4, 0, 1];
+  MAX_ROOMS = 4;
 }
 
 export {rewards, successes, videos, introVideoUrl, rooms, robot_strategy, roomOrder, roomList, MAX_ROOMS, TREATMENT, STRATEGY, greedy_choice, DEBUG_MODE, SITE_VERSION};
